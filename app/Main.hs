a061ce0d0abc74ac90c3b98ea5b7faf97d0ae2b2
fishyFrogFace <camillmg@stud.ntnu.no>
2017-08-02 01:45:41 +0200

    Image load/save (repl)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
+
+import System.Environment (getArgs)
+import Vision.Image
+import Vision.Image.Storage.DevIL (Autodetect (..), load, save)
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    [input, output] <- getArgs
+    image <- load Autodetect input
+    case image of
+        Right (rgb :: RGB) -> do
+            writeOut <- save Autodetect output rgb
+            case writeOut of
+                Nothing -> putStrLn "Image was successfully stored"
+                Just err -> do
+                    print err 
+        Left err -> do
+            print err
